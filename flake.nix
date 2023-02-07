1b8ebf60e4b09ac85a7e66c8906960dfb381bd29
Grigory Shipunov <blame@oxapentane.com>
2023-02-07 02:46:53 +0100

    devshell: something broken on unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     utils.url = "github:numtide/flake-utils";
     naersk = {
       url = "github:nix-community/naersk";
